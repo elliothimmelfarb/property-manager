a2f5b3584a6f04ba1a58ede42a795e5cfca0ac16
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-06-29 16:39:49 -0700

    initial commit

---
+++ public/js/module.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const app = angular.module('myApp', ['ui.router'])
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    // .state('welcome', {
+    //   url: '/',
+    //   templateUrl: '/html/welcome.html',
+    //   controller: 'welcomeController'
+    // })
+});
